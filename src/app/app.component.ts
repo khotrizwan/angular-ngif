4cd052e2ccf923a46f9f484c8a7a295d422a3b4d
Rizwan Khot <khotrizwan@gmail.com>
2021-06-10 22:48:03 +0530

    done with ngIf demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div *ngIf="true; else elseBlock">Text 1 to show</div>
+    <ng-template #elseBlock>Alternative text 1 to show</ng-template>
+
+    <div *ngIf="false; then thblock else elBlock"></div>
+    <ng-template #thBlock>Text 2 to show</ng-template>
+    <ng-template #elBlock>Alternative text 2 to show</ng-template>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
